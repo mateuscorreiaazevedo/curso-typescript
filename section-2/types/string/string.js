1509dec33503d4706edd56ff997285ef19f053e9
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-09 12:30:19 -0300

    funamentals: type string finish

---
+++ section-2/types/string/string.js
@@ -0,0 +1,7 @@
+var fistName = 'Mateus';
+// console.log(fistName.toUpperCase());
+var fullName;
+var lastName = 'Azevedo';
+fullName = fistName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
