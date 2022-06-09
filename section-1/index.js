91854174b328788b61a133ef4f62ee1ff233f1c2
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-09 14:03:28 -0300

    rename: aula-01 to section-1

---
+++ section-1/index.js
@@ -0,0 +1,9 @@
+var fistName = 'Mateus';
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log('Olá', name);
+}
+greeting(fistName);
+// greeting(anotherName)
+// greeting(x)
