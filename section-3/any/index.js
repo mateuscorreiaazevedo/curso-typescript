9d05313b68b8c9635768f750e7fee9613c4d7931
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-09 15:18:33 -0300

    lesson: use any and yours problems

---
+++ section-3/any/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Any transmite ao Typescript qualquer tipo de dado
+// Evitar ao máximo este tipo
+// 2 casos: tipo de dado não importante, arrays com dados de múltiplos tipos
+const list = [1, 'teste', true, [], { key: 1 }];
+console.log(list);
