e63507289f6445ac6870da625bf8624477922ea0
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-09 19:09:48 -0300

    lesson: determinig values and types for function parameters using literal types with union types

---
+++ section-3/literal-types/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let test;
+test = "testando";
+console.log(test);
+function showDirection(direction) {
+    console.log(`A direção da curva é ${direction}`);
+}
+showDirection("left");
