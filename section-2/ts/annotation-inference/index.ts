e9196fc2d7f43e5c81c21559f8c3f51440fdc2b2
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-09 13:13:44 -0300

    typescript insertion: understanding and applying ways to type data, annotation and inference

---
+++ section-2/ts/annotation-inference/index.ts
@@ -0,0 +1,13 @@
+// type annotation e type inference
+// Dois conceitos que vai aparecer em todo o processo de desenvolvimento de aplicações
+// Annotation -> Quando definimos o tipo de dado manualmente
+// Inference -> Quando o próprio TS identifica e define o tipo de dado automáticamente
+
+
+let annotation: string = 'teste'
+let inference = 10
+
+annotation = 'testeee'
+inference = 100
+inference.toExponential()
+annotation.toLowerCase()
