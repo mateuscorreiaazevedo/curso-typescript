4c366f1dad5fdaa21e7e49a9501e4571343a4982
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-10 14:03:57 -0300

    lesson: to determine void for functions without returns

--- section-5/functions/void/index.js
+++ section-5/functions/void/index.js
@@ -2,3 +2,7 @@
 // não retornam valores
 //  o tipo de dado é tipado como void
 // void -> não temos valor de retorno
+const unReturn = () => {
+    console.log(`essa função não tem retorno`);
+};
+unReturn();
--- section-5/functions/void/index.ts
+++ section-5/functions/void/index.ts
@@ -2,3 +2,8 @@
 //  o tipo de dado é tipado como void
 // void -> não temos valor de retorno
 
+const unReturn = (): void => {
+  console.log(`essa função não tem retorno`);
+
+}
+unReturn()
