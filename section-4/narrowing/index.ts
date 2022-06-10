73bcd66c932a26093d4d776b8ca4b851678034dd
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-10 11:07:36 -0300

    lesson: narrowing types

---
+++ section-4/narrowing/index.ts
@@ -0,0 +1,4 @@
+// é um recurso para identificar tipos de dados
+// dá uma direção diferente a execução do programa, baseada no tipo que foi identificado
+// tipos podem ser imprevisíveis
+// evitar erros do compilador
