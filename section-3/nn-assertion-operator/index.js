6822e6f84c31d91c0be774c2fe8797338be49e39
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-10 10:29:08 -0300

    lesson: using bigint and changing typescript target version in tsconfig.json

--- section-3/nn-assertion-operator/index.js
+++ section-3/nn-assertion-operator/index.js
@@ -3,4 +3,4 @@
 // às vezes o typescript pode evidenciar um erro, baseado em um valor que no ->
 // -> momento do código ainda não está disponível
 const p = document.getElementById('some-p');
-console.log(p === null || p === void 0 ? void 0 : p.innerText);
+console.log(p?.innerText);
