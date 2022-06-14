63d2a6e4530955c344d20c9df6b599039a0da302
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-14 14:15:24 -0300

    chore: using constraints on generic types for filter your output

--- section-7/generics/index.js
+++ section-7/generics/index.js
@@ -6,3 +6,13 @@ function showData(arg) {
 }
 console.log(showData(5));
 console.log(showData('testeeeee'));
+// CONSTRAINT EM GENERICS
+// Limitar os tipos aceitos no generic type
+// filtrar os tipos livres aceitos
+const showProductName = (obj) => {
+    return `O nome do produto é: ${obj.name}`;
+};
+const myObj = { name: "Camisa", color: "Preta lisa" };
+const myCar = { name: "Gol", color: "Preto", engine: '1.0' };
+console.log(showProductName(myObj));
+console.log(showProductName(myCar));
--- section-7/generics/index.ts
+++ section-7/generics/index.ts
@@ -7,3 +7,18 @@ function showData<T>(arg: T): string {
 
 console.log(showData(5));
 console.log(showData('testeeeee'));
+
+// CONSTRAINT EM GENERICS
+
+// Limitar os tipos aceitos no generic type
+// filtrar os tipos livres aceitos
+
+const showProductName = <T extends {name: string}> (obj: T) => {
+  return `O nome do produto é: ${obj.name}`
+}
+
+const myObj = {name: "Camisa", color: "Preta lisa"}
+const myCar = {name: "Gol", color: "Preto", engine: '1.0'}
+
+console.log(showProductName(myObj));
+console.log(showProductName(myCar));
