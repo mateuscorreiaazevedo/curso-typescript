c20a1ce4e325c94f6469463a6e511ecc74e264ef
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-14 19:54:12 -0300

    lesson: using a word reserved 'this'for defined a value and type of the field class

--- section-8/methods/index.js
+++ section-8/methods/index.js
@@ -12,3 +12,16 @@ class Dwarf {
 const jimmy = new Dwarf('Jimmy');
 console.log(jimmy.name);
 jimmy.greeting();
+class Truck {
+    constructor(model, hp) {
+        this.hp = hp;
+        this.model = model;
+    }
+    showDetails() {
+        return `O caminhão do modelo: ${this.model}, tem ${this.hp} cavalos de Potência`;
+    }
+}
+const mercedes = new Truck('mercedes', 540);
+const scania = new Truck('scania', 580);
+console.log(mercedes.showDetails());
+console.log(scania.showDetails());
--- section-8/methods/index.ts
+++ section-8/methods/index.ts
@@ -17,3 +17,24 @@ const jimmy = new Dwarf('Jimmy')
 console.log(jimmy.name);
 
 jimmy.greeting()
+
+
+class Truck {
+  model
+  hp
+
+  constructor(model: string, hp: number) {
+    this.hp = hp
+    this.model = model
+  }
+
+  showDetails(){
+    return `O caminhão do modelo: ${this.model}, tem ${this.hp} cavalos de Potência`
+  }
+}
+
+const mercedes = new Truck('mercedes', 540)
+const scania = new Truck('scania', 580)
+
+console.log(mercedes.showDetails())
+console.log(scania.showDetails())
