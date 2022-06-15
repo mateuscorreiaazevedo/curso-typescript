67c513a733d7c284bd85ff71edfb0794becdd113
mateuscorreiaazevedo <mateuscorreiaazevedo@gmail.com>
2022-06-15 17:09:20 -0300

    using is static word to define a static value on field

---
+++ section-8/static-members/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// atribuir o nome static ao campo da class
+class StaticMembers {
+}
+StaticMembers.prop = 'teste static';
+console.log(StaticMembers.prop);
---
+++ section-8/static-members/index.ts
@@ -0,0 +1,7 @@
+// atribuir o nome static ao campo da class
+
+class StaticMembers {
+  static prop = 'teste static'
+}
+
+console.log(StaticMembers.prop);
